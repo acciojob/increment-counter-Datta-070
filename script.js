7c27061c7072ca4b6c5fbd8ce8b97cfe76dd33fd
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-22 10:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let count=0;
+function btn(){
+	let pop=document.getElementById("counter");
+	if(count==0){
+	alert(count);
+	}
+	else{
+		alert(count-1);
+	}
+	count++;
+	document.getElementById("counter").innerText=count;
+}
